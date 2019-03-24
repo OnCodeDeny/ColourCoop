8f635a1d45c6758956c41e2e1c41d7ea2a5c86bf
Shane-sama <48694586+ShaneLogtenberg@users.noreply.github.com>
2019-03-24 14:26:53 -0400

    Doors?
    We got layers that let only certian players pass through. Don't know that to call them.

---
+++ Colours/Assets/Code/Doors.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    [Range(1, 2)]
+    public int colorid;
+    public string playerTag;
+    Collider2D c2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c2d = GetComponent<Collider2D>();
+        if (colorid == 1)
+        {
+            playerTag = "Player1";
+        }
+        if (colorid == 2)
+        {
+            playerTag = "Player2";
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag != playerTag)
+        {
+
+        }
+    }
+}
