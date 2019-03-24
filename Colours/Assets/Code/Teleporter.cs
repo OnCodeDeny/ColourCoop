4f22f099c03f0f65f315c6f25d41555a5fc650f1
LEOLEOW <48531116+LEOLEOW@users.noreply.github.com>
2019-03-24 06:35:43 -0400

    teleporter
    trigger based teleporter

---
+++ Colours/Assets/Code/Teleporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject waypoint;
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if ((other.gameObject.tag == "Player1") && Input.GetButtonDown("P1Teleport"))
+        {
+                other.transform.position = new Vector2(waypoint.transform.position.x, waypoint.transform.position.y);
+        }
+
+        if ((other.gameObject.tag == "Player2") && Input.GetButtonDown("P2Teleport"))
+        {
+            other.transform.position = new Vector2(waypoint.transform.position.x, waypoint.transform.position.y);
+        }
+    }
+
+
+}
