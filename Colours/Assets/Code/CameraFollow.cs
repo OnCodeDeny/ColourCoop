6694b2d0038427375728935e3deff75dfd10f7eb
Shane-sama <48694586+ShaneLogtenberg@users.noreply.github.com>
2019-03-23 20:18:22 -0400

    Camera
    Camera's that follow the player with soothdamp

---
+++ Colours/Assets/Code/CameraFollow.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public PlayerMovement target;
+    public float HorizontalDstX;
+    public float VerticalDstX;
+    public float HorizontalSmoothTime;
+    public float verticalSmoothTime;
+    public Vector2 focusAreaSize;
+
+    FocusArea focusArea;
+
+    float currentHorizontalX;
+    float currentVerticalX;
+    float targetHorizontalX;
+    float targetVerticalX;
+    float HorizontalDirX;
+    float VerticalDirX;
+    float smoothLookVelocityX;
+    float smoothLookVelocityY;
+
+    bool lookAheadStopped;
+
+
+    private void Start()
+    {
+        focusArea = new FocusArea(target.c2d.bounds, focusAreaSize);
+    }
+
+    private void LateUpdate()
+    {
+        focusArea.Update(target.c2d.bounds);
+
+        Vector2 focusPostion = focusArea.center;
+
+        if (focusArea.velocity.x != 0)
+        {
+            HorizontalDirX = Mathf.Sign(focusArea.velocity.x);
+            if (Mathf.Sign(target.inputDirection.x) == Mathf.Sign(focusArea.velocity.x) && target.inputDirection.x != 0)
+            {
+                lookAheadStopped = false;
+                targetHorizontalX = HorizontalDirX * HorizontalDstX;
+            }
+            else
+            {
+                if (!lookAheadStopped)
+                {
+                    lookAheadStopped = true;
+                    targetHorizontalX = currentHorizontalX + (HorizontalDirX * HorizontalDstX - currentHorizontalX) / 4f;
+                }
+            }
+        }
+
+        if (focusArea.velocity.y != 0)
+        {
+            VerticalDirX = Mathf.Sign(focusArea.velocity.y);
+            if (Mathf.Sign(target.inputDirection.y) == Mathf.Sign(focusArea.velocity.y) && target.inputDirection.y != 0)
+            {
+                lookAheadStopped = false;
+                targetVerticalX = VerticalDirX * VerticalDstX;
+            }
+            else
+            {
+                if (!lookAheadStopped)
+                {
+                    lookAheadStopped = true;
+                    targetVerticalX = currentVerticalX + (VerticalDirX * VerticalDstX - currentVerticalX) / 4f;
+                }
+            }
+        }
+
+        currentHorizontalX = Mathf.SmoothDamp(currentHorizontalX, targetHorizontalX, ref smoothLookVelocityX, HorizontalSmoothTime);
+        currentVerticalX = Mathf.SmoothDamp(currentVerticalX, targetVerticalX, ref smoothLookVelocityY, verticalSmoothTime);
+
+        focusPostion += new Vector2(1 * currentHorizontalX, 1 * currentVerticalX);
+
+        transform.position = (Vector3)focusPostion + Vector3.forward * -10;
+
+        transform.position = (Vector3)focusPostion + Vector3.forward * -10;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1, 0.5f);
+        Gizmos.DrawCube(focusArea.center, focusAreaSize);
+    }
+
+    struct FocusArea
+    {
+        public Vector2 center;
+        public Vector2 velocity;
+        float left, right;
+        float top, bottom;
+
+
+        public FocusArea(Bounds targetBounds, Vector2 size)
+        {
+            left = targetBounds.center.x - size.x / 2;
+            right = targetBounds.center.x + size.x / 2;
+            bottom = targetBounds.center.x - size.x / 2;
+            top = targetBounds.center.x + size.x / 2;
+
+            velocity = Vector2.zero;
+            center = new Vector2((left + right) / 2, (top + bottom) / 2);
+        }
+
+        public void Update(Bounds targetBounds)
+        {
+            float shiftX = 0;
+            if (targetBounds.min.x < left)
+            {
+                shiftX = targetBounds.min.x - left;
+            }
+            else if (targetBounds.max.x > right)
+            {
+                shiftX = targetBounds.max.x - right;
+            }
+            left += shiftX;
+            right += shiftX;
+
+            float shiftY = 0;
+            if (targetBounds.min.y < bottom)
+            {
+                shiftY = targetBounds.min.y - bottom;
+            }
+            else if (targetBounds.max.y > top)
+            {
+                shiftY = targetBounds.max.y - top;
+            }
+            bottom += shiftY;
+            top += shiftY;
+            center = new Vector2((left + right) / 2, (top + bottom) / 2);
+            velocity = new Vector2(shiftX, shiftY);
+        }
+    }
+}
--- Colours/Assets/Code/PlayerMovement.cs
+++ Colours/Assets/Code/PlayerMovement.cs
@@ -14,16 +14,18 @@ public class PlayerMovement : MonoBehaviour
     private float decelleration = 100f;
 
     private Rigidbody2D rb;
+    public Collider2D c2d;
     private Vector3 eulerAngles;
     private Player player;
     private string horaxis;
     private string veraxis;
-       
+    public Vector2 inputDirection = Vector2.zero;
 
     private void OnEnable()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GetComponent<Player>();
+        c2d = GetComponent<Collider2D>();
         rb.freezeRotation = true;
 
         if(player.playerid == 1)
@@ -40,7 +42,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 inputDirection = Vector2.zero;
         inputDirection = new Vector2(Input.GetAxisRaw(horaxis), Input.GetAxisRaw(veraxis));
         //inputDirection = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * inputDirection;
         //inputDirection.Normalize();
@@ -62,5 +63,20 @@ public class PlayerMovement : MonoBehaviour
         Vector3 euler = transform.eulerAngles;
         euler.y = Mathf.LerpAngle(euler.y, eulerAngles.y, Time.deltaTime * 15f);
         transform.eulerAngles = euler;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool north, south;
+        public bool west, east;
+
+        public Vector2Int faceDir;
+
+        public void Reset()
+        {
+            north = south = false;
+            west = east = false;
+
+        }
     }
 }
