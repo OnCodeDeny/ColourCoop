80951ed2598fb8ab6e51f778ebf5df58e11ecda0
Leann Sterling <leann.sterling@hotmail.com>
2019-03-31 17:02:02 -0400

    Start Screen Added

---
+++ Colours/Assets/Code/SceneChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string SceneName;
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
