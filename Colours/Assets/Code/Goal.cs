a11aca628e410578b44518eef069d8d1b8da6e45
Leann Sterling <leann.sterling@hotmail.com>
2019-03-31 16:41:31 -0400

    Level designed!!!

---
+++ Colours/Assets/Code/Goal.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool RedPresent;
+    public bool BluePresent;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player1")
+        {
+            RedPresent = true;
+            Debug.Log("Red");
+        }
+        if (other.gameObject.tag == "Player2")
+        {
+            BluePresent = true;
+            Debug.Log("Blue");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player1")
+        {
+            RedPresent = false;
+        }
+        if (other.gameObject.tag == "Player2")
+        {
+            BluePresent = false;
+            Debug.Log("Off");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (RedPresent == true && BluePresent == true)
+        {
+            Debug.Log("Next scene");
+        }
+    }
+}
---
+++ Colours/Assets/Code/Switch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public SpriteRenderer wallColour;
+    public GameObject wall;
+    //private Renderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (wall.layer == 11)
+        {
+            if ((other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2") && Input.GetButtonDown("SwitchColour"))
+            {
+                wall.layer = 10;
+                wallColour.color = new Color(1, 0.3254f, 0.3254f, 1);
+            }
+        }
+        else if (wall.layer == 10)
+        {
+            if ((other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2") && Input.GetButtonDown("SwitchColour"))
+            {
+                wall.layer = 11;
+                wallColour.color = new Color(0.3515f, 0.3254f, 1, 1);
+            }
+        }
+    }
+}
