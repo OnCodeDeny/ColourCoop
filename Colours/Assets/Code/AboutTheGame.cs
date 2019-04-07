ecf62dbd5d91807f5453c7cfd5afe1be70e528f9
Leann Sterling <leann.sterling@hotmail.com>
2019-04-07 17:16:00 -0400

    Started Audio Implementation

---
+++ Colours/Assets/Code/AboutTheGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutTheGame : MonoBehaviour
+{
+    public GameObject AboutScreen;
+
+    public void ShowInfo()
+    {
+        Instantiate(AboutScreen);
+    }
+}
