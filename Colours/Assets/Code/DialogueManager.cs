119672e65b0a9d322c11d7a53f328a07b04ab38a
Leann Sterling <leann.sterling@hotmail.com>
2019-04-15 02:22:52 -0400

    Voice and music

---
+++ Colours/Assets/Code/DialogueManager.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public void Line1()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.s");
+    }
+
+    public void Line2()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.1");
+    }
+
+    public void Line3()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.2");
+    }
+
+    public void Line4()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.3");
+    }
+
+    public void Line5()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.4");
+    }
+
+    public void Line6()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.5");
+    }
+
+    public void Line7()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.6");
+    }
+
+    public void Line8()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.7");
+    }
+
+    public void Line9()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 1 Dialogue/1.8");
+    }
+
+    public void Line10()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 2 Dialogue/2.s");
+    }
+
+    public void Line11()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 2 Dialogue/2.1");
+    }
+
+    public void Line12()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 2 Dialogue/2.2");
+    }
+
+    public void Line13()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 2 Dialogue/2.3");
+    }
+
+    public void Line14()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 2 Dialogue/2.4");
+    }
+
+    public void Line15()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 3 Dialogue/3.s");
+    }
+
+    public void Line16()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 3 Dialogue/3.1");
+    }
+
+    public void Line17()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 3 Dialogue/3.2");
+    }
+
+    public void Line18()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 3 Dialogue/3.3");
+    }
+
+    public void Line19()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 3 Dialogue/3.4");
+    }
+
+    public void Line20()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 4 Dialogue/4.s");
+    }
+
+    public void Line21()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 4 Dialogue/4.1");
+    }
+
+    public void Line22()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 4 Dialogue/4.2");
+    }
+
+    public void Line23()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 4 Dialogue/4.3");
+    }
+
+    public void Line24()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 4 Dialogue/4.4");
+    }
+
+    public void Line25()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.s");
+    }
+
+    public void Line26()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.1");
+    }
+
+    public void Line27()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.2");
+    }
+
+    public void Line28()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.3");
+    }
+
+    public void Line29()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.4");
+    }
+
+    public void Line30()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.5");
+    }
+
+    public void Line31()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.6");
+    }
+
+    public void Line32()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.7");
+    }
+
+    public void Line33()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 5 Dialogue/5.8");
+    }
+
+    public void Line34()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 6 Dialogue/6.s");
+    }
+
+    public void Line35()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 6 Dialogue/6.1");
+    }
+
+    public void Line36()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 6 Dialogue/6.2");
+    }
+
+    public void Line37()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 6 Dialogue/6.3");
+    }
+
+    public void Line38()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Scene 6 Dialogue/6.4");
+    }
+}
